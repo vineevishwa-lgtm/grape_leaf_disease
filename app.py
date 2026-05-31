ed0d2c02197f49cb32ec5f847dc8b27592d7a532
vineevishwa-lgtm <vineevishwa@gmail.com>
2026-05-31 22:27:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+from tensorflow.keras.models import load_model
+from PIL import Image
+import numpy as np
+import os
+import gdown
+
+app = Flask(__name__)
+
+# Download model from Google Drive if not present
+MODEL_PATH = "grape_model.h5"
+GDRIVE_ID = "1qqmmBmnHZRYjvOZYKOvIUWHzkrJ2kL8o"
+
+if not os.path.exists(MODEL_PATH):
+    print("Downloading model from Google Drive...")
+    gdown.download(f"https://drive.google.com/uc?id={GDRIVE_ID}", MODEL_PATH, quiet=False)
+
+# Load model
+model = load_model(MODEL_PATH)
+
+classes = ["Early", "Healthy", "Moderate", "Severe"]
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    file = request.files['image']
+    img = Image.open(file).convert("RGB")
+    img = img.resize((224, 224))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    prediction = model.predict(img_array)
+    class_index = np.argmax(prediction)
+    confidence = np.max(prediction) * 100
+    result = classes[class_index]
+    return render_template(
+        "index.html",
+        prediction=result,
+        confidence=round(confidence, 2)
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
